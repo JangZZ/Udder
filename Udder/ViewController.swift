0a4f512fac3a85e53449ba1249b8948826804dd7
Ngô Giang <jangz@ngos-macbook-pro.local>
2018-11-22 06:10:53 +0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ngô Trường Giang on 11/22/18.
+//  Copyright © 2018 Ngô Trường Giang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
